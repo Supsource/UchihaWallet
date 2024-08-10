41c23136d3f053b6daa35cd2942a236dd64b93d8
Supriyo <supriyomalstartup@gmail.com>
2024-08-10 10:51:11 +0530

    added ethers and setup

---
+++ src/utils/AccountUtils.ts
@@ -0,0 +1,19 @@
+import { ethers } from "ethers";
+
+export const generateKeys = (seedPhrase?: string) => {
+    let wallet: ethers.Wallet;
+
+    if(seedPhrase){
+        wallet = ethers.Wallet.fromMnemonic(seedPhrase);
+    }else{
+        wallet = ethers.Wallet.createRandom();
+        seedPhrase = wallet.mnemonic.phrase;
+    }
+
+    const privateKey = wallet.privateKey;
+    const address = wallet.address;
+
+
+
+    return {seedPhrase, privateKey, address};
+}    
